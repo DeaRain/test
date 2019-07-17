75f245d98d9a7aae38247faab8f2df76667661bf
DeaRain <steallmaster@gmail.com>
2019-07-18 01:08:13 +0300

    Victory!

--- html/app/js/common.js
+++ html/app/js/common.js
@@ -10,7 +10,43 @@ $( document ).ready(function() {
         arrows:true,
         slidesToShow: 4,
         slidesToScroll: 2,
-        variableWidth: true
+        // variableWidth: true,
+        responsive: [
+            {
+                breakpoint: 1199,
+                settings: {
+                    dots:true,
+                    arrows:true,
+                    slidesToShow: 3,
+                    slidesToScroll: 2,
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    dots:true,
+                    arrows:true,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    dots:true,
+                    arrows:true,
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+            ]
     });
 });
 
