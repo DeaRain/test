2a35f5e1da6a760831654a20d0c58dcbf595a64d
DeaRain <steallmaster@gmail.com>
2019-07-30 13:16:54 +0300

    a

--- html/app/js/common.js
+++ html/app/js/common.js
@@ -3,6 +3,152 @@ console.log('common file');
 $( document ).ready(function() {
 
 
+    if($(window).width()<993) {
+        var drop_menu = $('.header-container__right-drop-lists').detach();
+        drop_menu.appendTo('.left-menu__top-block');
+    }
+    $('.infoline-slider-js').slick({
+        // dots:true,
+        arrows:false,
+        slidesToShow: 6,
+        slidesToScroll: 3,
+        variableWidth: true,
+        // draggable: true
+        responsive: [
+            {
+                breakpoint: 1199,
+                settings: {
+                    infinity: true,
+                    variableWidth: false,
+                    centerPadding: true,
+                    centerMode: true,
+                    arrows:true,
+                    slidesToShow: 4,
+                    slidesToScroll: 3,
+                }
+            },
+            {
+                breakpoint: 993,
+                settings: {
+                    infinity: true,
+                    variableWidth: false,
+                    centerPadding: true,
+                    centerMode: true,
+                    arrows:true,
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                }
+            },
+            {
+                breakpoint: 767,
+                settings: {
+                    infinity: true,
+                    variableWidth: false,
+                    centerPadding: true,
+                    centerMode: true,
+                    arrows:true,
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                }
+            },
+            {
+                breakpoint: 479,
+                settings: {
+                    infinity: true,
+                    variableWidth: false,
+                    centerPadding: true,
+                    centerMode: true,
+                    arrows:true,
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+            }
+        ]
+    });
+
+
+    $('.production-slider').slick({
+        dots:true,
+        arrows:true,
+        slidesToShow: 4,
+        slidesToScroll: 2,
+        variableWidth: true,
+        responsive: [
+            {
+                breakpoint: 1199,
+                settings: {
+                    dots:true,
+                    arrows:true,
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    variableWidth: false,
+                    centerPadding: true,
+                    centerMode: true,
+                }
+            },
+            {
+                breakpoint: 993,
+                settings: {
+                    dots:true,
+                    arrows:true,
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    variableWidth: true,
+                }
+            },
+            {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    variableWidth: false,
+                    centerPadding: true,
+                    centerMode: true,
+                }
+            }
+        ]
+    });
+
+
+    $('.menu-item').click(function (e) {
+        if($(this).parent().is('.left-menu')) {
+            if (e.target.getAttribute('href')=="javascript:void(0);") {
+                $(this).find('.sub-menu').toggle(50);
+                $(this).children('a').toggleClass('after-rotate');
+            }
+        }
+        if($(this).parent().is('.menu-footer')) {
+            if($(window).width()<768) {
+                if (e.target.getAttribute('href') == "javascript:void(0);") {
+                    $(this).find('.sub-menu').toggle(50);
+                    $(this).children('a').toggleClass('after-rotate');
+                    if ($(this).parent().is('.menu-footer-contact')) {
+                        $(this).parent().parent().find('.footer_container__toggle').toggle(50);
+                    }
+
+                    if ($(this).parent().is('.menu-footer-client')) {
+                        $(this).parent().parent().find('.contact-toogle').toggle(50);
+                    }
+                }
+            }
+        }
+    });
+
+
+    $('.top-menu-changer').click(function () {
+        if($(window).width()<993) {
+            $(this).parent().find('.top-menu').toggle(50);
+            $(this).children().toggleClass('after-rotate');
+        }
+    });
+
+    $('.top-menu__item-url').click(function (e) {
+        if($(window).width()<993) {
+            $(this).parent().find('.top-menu__item-container').toggle(50);
+            $(this).parent().toggleClass('after-rotate');
+        }
+    });
+
         $('.right-drop-list').click(function () {
             $(this).find('.right-drop-list__container').toggleClass('right-drop-list__container_active');
         });
@@ -51,32 +197,7 @@ $( document ).ready(function() {
 
 
 
-    $('.production-slider').slick({
-        dots:true,
-        arrows:true,
-        slidesToShow: 4,
-        slidesToScroll: 2,
-        variableWidth: true,
-        responsive: [
-            {
-                breakpoint: 1199,
-                settings: {
-                    dots:true,
-                    arrows:true,
-                    slidesToShow: 3,
-                    slidesToScroll: 2,
-                    variableWidth: true,
-                }
-            },
-            {
-                breakpoint: 767,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-            }
-            ]
-    });
+
 });
 
 // $(document).ready( function() {
